016902ee49d1e87b1c6cb56a998f94a29098a0ce
Maman Lawaly BOUKARI <mlawaly@gmail.com>
2021-05-20 13:13:23 +0000

    Commit apres dependance

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -3,6 +3,7 @@ object ExamenBigData {
     println("Fonction principale")
     val l1 : List[String] = List("julien", "Paul", "jean", "rac", "trec", "joel", "ed", "chris", "maurice")
     println(renvoiListe(l1))
+    liste_double()
   }
 
   def renvoiListe(list : List[String]): List[String] = {
@@ -10,6 +11,17 @@ object ExamenBigData {
     return list2
   }
 
-
+  def liste_double(): Unit ={
+    val  maListe = List(List("ecommercemar.fr"," "),List("https://www.journalducm.com/contact/","Payant"),
+      List("https://www.zatsaz.com/",""),List("https://www.lerevenu.com",""),
+      List("https://www.cadre-dirigeant-magazine.com/","payant"),
+      List("https://www.silicon.fr/services/contact#annoncer/","Payant"),
+      List("https://www.channelbiz.fr/nous-contacter/",""),
+      List("https://www.itespresso.fr/",""),
+      List("https://www.industrie-mag.com/article4.html","invite"),
+      List("https://www.jesuisundev.com/article-invite/","invite"),
+      List("https://www.numerama.com/",""))
+    maListe.foreach(l=>{println(l(0)+" "+l(1))})
+  }
 
 }
