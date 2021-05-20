f3ca097d882273071df85c5a8c4d9d1a4e9095a1
Maman Lawaly BOUKARI <mlawaly@gmail.com>
2021-05-20 17:27:46 +0000

    Commit final

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,13 +1,14 @@
 object ExamenBigData {
   def main(args: Array[String]): Unit = {
-    println("Fonction principale")
+
     val l1 : List[String] = List("julien", "Paul", "jean", "rac", "trec", "joel", "ed", "chris", "maurice")
-    println(renvoiListe(l1))
+    renvoiListe(l1)
     liste_double()
   }
 
   def renvoiListe(list : List[String]): List[String] = {
     val list2 = list.filter(s => (s.charAt(s.length-1) == 'n'))
+    list2.foreach(l =>println(l))
     return list2
   }
 
